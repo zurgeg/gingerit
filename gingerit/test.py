49a77eb72153636b773f342b49d20899b212f556
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-08-05 22:54:05 +0200

    Added first test case

---
+++ gingerit/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from gingerit import GingerIt
+
+
+class TestGingerIt(unittest.TestCase):
+    def test(self):
+        text = 'The smelt of fliwers bring back memories.'
+
+        parser = GingerIt()
+        output = parser.parse(text)
+        self.assertEqual(output.get("result"), "The smell of flowers brings back memories")
+
+if __name__ == '__main__':
+    unittest.main()
