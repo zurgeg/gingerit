6098ccdd71c9ba7872a051b2a3b4320d0abbc0d2
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2018-10-29 20:58:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    "requests==2.8.1"
+    "requests"
 ]
 
 test_requirements = [
